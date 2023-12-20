549e4df8ff7553d7ab1d83c1a817d38c2e478506
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-12-20 20:04:42 +0000

    Migrated, missing the rest of endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,77 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#Add new user
+@app.route('/user', methods=['POST'])
+def handle_new_user():
+    req_body = request.get_json ()
+    user_email = req_body ['email']
+    user_password = req_body ['password']
+    user_username = req_body ['username']
+    user_is_active = req_body['is_active']
+
+    new_user = User (email=user_email, password=user_password, is_active=user_is_active, username=user_username)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify ({'Message': 'Created a new user'}), 200
+
+# gets a list of all the users in the database
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+    
+    if len(serialized_users) > 0:
+        return jsonify(serialized_users), 200
+    return jsonify ({'Message': 'No users in database'}), 404
+    
+# gets a list of all the people in the database
+@app.route('/people', methods=['GET'])
+def get_people():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# Adds a new person to people
+@app.route('/person', methods=['POST'])
+def add_person():
 
-    return jsonify(response_body), 200
+# gets a single person from the database through ID
+@app.route('/person/<int:person_id>', methods=['GET'])
+def get_person(person_id):
+
+# gets all the favorites that belong to the user ID
+@app.route('/users/favorite/<int:user_id>', methods=['GET'])
+def get_all_favorites (user_id):
+    
+# adds a new planet
+@app.route('/planets', methods=['POST'])
+def add_planet():
+
+# gets the list of all planets
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+# gets a certain planet ID info
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet():
+
+# add a new favorite planet to the current user 
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet (planet_id):
+
+# add a new favorite person to the current user 
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_person (person_id):
+
+#deletes favorite planet with the id = planet_id
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet (planet_id):
+
+#deletes favorite people with the id = people_id
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_person (person_id):
+
+    
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,25 +5,29 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column (db.String (20),unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.email
+        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
 
-class Favorites (db.Model):
-    __tablename__ = 'favorites'
+class Favorite (db.Model):
+    __tablename__ = 'favorite'
     id = db.Column (db.Integer, primary_key=True)
     user_id = db.Column (db.Integer, db.ForeignKey('user.id'))
-    planet_id = db.Column (db.Integer,db.ForeignKey('planet.id'),nullable=True)
+    planet_id = db.Column (db.Integer,db.ForeignKey('planets.id'),nullable=True)
     people_id = db.Column (db.Integer,db.ForeignKey('people.id'),nullable=True)
 
 
